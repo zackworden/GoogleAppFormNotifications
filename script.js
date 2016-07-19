f33260a6032928d6899159e535218c814deba284
Zack Worden <zworden@ehpub.com>
2016-07-19 15:42:07 -0400

    Initial commit!

---
+++ script.js
@@ -0,0 +1,19 @@
+// notified services
+	// email address to mail to
+
+// form service
+	// form name
+	// responses to all questions
+
+// email service
+	// email address
+	// subject line
+	// body
+
+/*
+PROCESS:
+get form answers
+create an email and send to each notified service in a queue
+allow form answers to flow into google sheet
+
+*/
